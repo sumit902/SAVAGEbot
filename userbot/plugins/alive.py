fb600d10f402f8deb217fcc563080ef367281429
Sensei <67994849+SenseiOfficial@users.noreply.github.com>
2020-11-18 22:50:20 +0530

    Update alive.py
    🔻🔻🔻 ABB chalega

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -10,14 +10,14 @@ from telethon.tl.types import ChannelParticipantsAdmins
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
 PM_IMG = "https://media.giphy.com/media/Lopx9eUi34rbq/giphy.gif"
 pm_caption = "`ᔕᗴᑎᔕᗴᎥᗰᗩ᙭-Kingbot IS:` **ONLINE**\n\n"
-pm_caption += "**SYSTEM STATUS**\n"
-pm_caption += "`TELETHON VERSION:` **6.0.9**\n`Python:` **3.7.4**\n"
-pm_caption += "`DATABASE STATUS:` **Functional**\n"
-pm_caption += "**Current Branch** : `master`\n"
-pm_caption += "**ᔕᗴᑎᔕᗴᎥᗰᗩ᙭-Kingbot OS** : `1.14`\n"
-pm_caption += f"**My Boss** : {DEFAULTUSER} \n"
-pm_caption += "**Made By 😎** : [Sensei](https://t.me/sensei_nex)\n\n"
-pm_caption += "Deploy Your Own : [Repo](https://github.com/SenseiMAX/SenseiMAX-Kingbot)\n"
+pm_caption += "🔸**SYSTEM STATUS**\n"
+pm_caption += "🔹TELETHON VERSION             : **6.0.9**\n`Python:` **3.7.4**\n"
+pm_caption += "🔸DATABASE STATUS              : **Functional**\n"
+pm_caption += "🔹**Current Branch**           : `Master`\n"
+pm_caption += "🔸**ᔕᗴᑎᔕᗴᎥᗰᗩ᙭-Kingbot OS**  : `1.14`\n"
+pm_caption += f"🔹**My Boss**                 : {DEFAULTUSER} \n"
+pm_caption += "🔸**Made By 😎**               : [Sensei](https://t.me/sensei_nex)\n\n"
+pm_caption += "🔻Deploy Your Own              : [Repo](https://github.com/SenseiMAX/SenseiMAX-Kingbot)\n"
 
 @borg.on(admin_cmd(pattern=r"alive"))
 async def friday(alive):
