48bcc981945f77d69cde592f5fdacac7beaad845
Sensei <67994849+SenseiOfficial@users.noreply.github.com>
2020-11-18 13:46:37 +0530

    Update alive.py
    abb kaam krega

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -8,19 +8,19 @@ from userbot.utils import admin_cmd
 
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
-ALIVE_IMG = "https://telegra.ph/file/7f0046214f2331d691679.jpg"
-ALIVE_caption = "`Sensible Userbot IS:` **ONLINE**\n\n"
+ALIVE_IMG = "https://tenor.com/9OER.gif"
+ALIVE_caption = "`ᔕᗴᑎᔕᗴᎥᗰᗩ᙭-Kingbot IS:` **🔥on Fire🔥**\n\n"
 ALIVE_caption += "**SYSTEM STATUS**\n\n"
 ALIVE_caption += "`TELETHON VERSION:` **6.0.9**\n`Python:` **3.7.4**\n\n"
 ALIVE_caption += "`DATABASE STATUS:` **Functional**\n\n"
 ALIVE_caption += "**Current Branch** : `master`\n\n"
-ALIVE_caption += "**Sensible  OS** : `3.14`\n\n"
-ALIVE_caption += "**Current Sat** : `Sensible Userbot Sat-2.95`\n\n"
+ALIVE_caption += "**ᔕᗴᑎᔕᗴᎥᗰᗩ᙭-Kingbot  OS** : `1.0`\n\n"
+ALIVE_caption += "**Current Sat** : `ᔕᗴᑎᔕᗴᎥᗰᗩ᙭-Kingbot 1.0`\n\n"
 ALIVE_caption += f"**My Boss** : {DEFAULTUSER} \n\n"
 ALIVE_caption += "**Heroku Database** : `AWS - Working Properly`\n\n"
-ALIVE_caption += "**Bot Made By @ceowhitehatcracks** \n\n"
-ALIVE_caption += "Copyright By [CEOWHITEHATCRACKS](GitHub.com/spandey112)\n\n"
-ALIVE_caption += "[Deploy SensibleUserbot](GitHub.com/spandey112/SensibleUserbot)"
+ALIVE_caption += "**Bot Made By @Sensei_nex** \n\n"
+ALIVE_caption += "Copyright By [Sensei](https://t.me/sensei_nex)\n\n"
+ALIVE_caption += "[Deploy ᔕᗴᑎᔕᗴᎥᗰᗩ᙭-Kingbot](https://github.com/SenseiMAX/SenseiMAX-Kingbot)"
 #@command(outgoing=True, pattern="^.alive$")
 @borg.on(admin_cmd(pattern=r"alive"))
 async def sensible(alive):
