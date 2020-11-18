e8348cf75bac921ffc6c55c3af72f59aac941422
Sensei <67994849+SenseiOfficial@users.noreply.github.com>
2020-11-18 10:31:26 +0530

    ᔕᗴᑎᔕᗴᎥᗰᗩ᙭-Kingbot
    10:31 prr kiya

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -41,7 +41,8 @@ async def cmd_list(event):
             else:
                 await event.edit(input_str + " is not a valid plugin!")
         else:
-            help_string = """Userbot Helper.. \nProvided by [@Sensible_userbot](https://t.me/sensible_userbot)\n`Userbot Helper to reveal all the commands`"""
+            help_string = """Userbot Helper.. \nProvided by [ᔕᗴᑎᔕᗴᎥᗰᗩ᙭-Kingbot](https://t.me/SenseiMAXprojectserbot)\n`→𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐇𝐞𝐥𝐩𝐞𝐫 𝐢𝐧 𝐚𝐬𝐬𝐢𝐬𝐭𝐢𝐧𝐠 𝐲𝐨𝐮 𝐡𝐨𝐰 𝐭𝐨 𝐮𝐬𝐞 𝐭𝐡𝐞 𝐛𝐨𝐭 𝐚𝐧𝐝 𝐭𝐨 𝐫𝐞𝐯𝐞𝐚𝐥 𝐚𝐥𝐥 𝐭𝐡𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬 𝐲𝐨𝐮 𝐫𝐞𝐪𝐮𝐢𝐫𝐞
+→`"""
             results = await bot.inline_query(  # pylint:disable=E0602
                 tgbotusername,
                 help_string
@@ -60,7 +61,7 @@ async def _(event):
     plugin_name = event.pattern_match.group(1)
     if plugin_name in borg._plugins:
         help_string = borg._plugins[plugin_name].__doc__
-        unload_string = f"Use `.unload {plugin_name}` to remove this plugin.\n           © @Sensible_userbot"
+        unload_string = f"Use `.unload {plugin_name}` to remove this plugin.\n           © @ᔕᗴᑎᔕᗴᎥᗰᗩ᙭-Kingbot"
         if help_string:
             plugin_syntax = f"Syntax for plugin **{plugin_name}**:\n\n{help_string}\n{unload_string}"
         else:
