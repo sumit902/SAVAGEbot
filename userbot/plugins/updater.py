b68479ac3f007be550471801629395617744734f
Sensei <67994849+SenseiOfficial@users.noreply.github.com>
2020-11-18 23:15:37 +0530

    Update updater.py
    luxury effets babe

--- userbot/plugins/updater.py
+++ userbot/plugins/updater.py
@@ -16,7 +16,7 @@ IS_SELECTED_DIFFERENT_BRANCH = (
     "please check out to an official branch, and re-start the updater."
 )
 OFFICIAL_UPSTREAM_REPO = "https://github.com/SenseiMAX/SenseiMAX-Kingbot"
-BOT_IS_UP_TO_DATE = "ᔕᗴᑎᔕᗴᎥᗰᗩ᙭-Kingbo is up-to-date!"
+BOT_IS_UP_TO_DATE = "❂ᔕᗴᑎᔕᗴᎥᗰᗩ᙭-Kingbo is up-to-date!❂"
 NEW_BOT_UP_DATE_FOUND = (
     "**ᔕᗴᑎᔕᗴᎥᗰᗩ᙭-Kingbo Update Found For** {branch_name}\n"
     "\n\n{changelog}\n"
@@ -72,7 +72,7 @@ async def updater(message):
     )
 
     if not changelog:
-        await message.edit("**Updating ᔕᗴᑎᔕᗴᎥᗰᗩ᙭-Kingbot* \n**Version** : `2.0` \n**Telethon** : `1.15.0` \n**Status** : `Pulling Updates` \n**Thank You For Using ᔕᗴᑎᔕᗴᎥᗰᗩ᙭-Kingbot !**")
+        await message.edit("**﹁﹁Updating ᔕᗴᑎᔕᗴᎥᗰᗩ᙭-Kingbot* \n**Version** : `2.0` \n**Telethon** : `1.15.0` \n**Status** : `«Pulling Updates»` \n『**Thank You For Using ᔕᗴᑎᔕᗴᎥᗰᗩ᙭-Kingbot !**』")
         await asyncio.sleep(5)
  
     message_one = NEW_BOT_UP_DATE_FOUND.format(
@@ -140,7 +140,7 @@ def generate_change_log(git_repo, diff_marker):
 
 async def deploy_start(bot, message, refspec, remote):
     await message.edit(RESTARTING_APP)
-    await message.edit("**Updating ᔕᗴᑎᔕᗴᎥᗰᗩ᙭-Kingbot** \n**Version** : `2.0` \n**Telethon** : `1.15.0` \n**Branch** : `Master` \n**Status** : `Updating & Restarting` \n__You Can Do__ `.alive` __To Check If I am Alive !__")
+    await message.edit("**✇✇Updating ᔕᗴᑎᔕᗴᎥᗰᗩ᙭-Kingbot✇** \n**Version** : `2.0` \n**Telethon** : `1.15.0` \n**Branch** : `✇Master✇` \n**Status** : `✇Updating & Restarting` \n__You Can Do__ `.alive` __To Check If I am Alive !__✇")
     remote.push(refspec=refspec)
     await bot.disconnect()
     os.execl(sys.executable, sys.executable, *sys.argv)
