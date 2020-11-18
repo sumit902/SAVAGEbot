be89d109a13d18b6c0a8ecfd38fac9a904a5f8e4
Sensei <67994849+SenseiOfficial@users.noreply.github.com>
2020-11-18 15:09:44 +0530

    updated tag
    updated tag

--- userbot/plugins/autobio.py
+++ userbot/plugins/autobio.py
@@ -15,7 +15,7 @@ async def _(event):
     while True:
         DMY = time.strftime("%d.%m.%Y")
         HM = time.strftime("%H:%M:%S")
-        bio = f"📅 {DMY} | I am A Pro 😎 | ⌚️ {HM}"
+        bio = f"📅 {DMY} | I am A Pro 😎 #ᔕᗴᑎᔕᗴᎥᗰᗩ᙭ ᛕᎥᑎǤᗷᗝ丅USER| ⌚️ {HM}"
         logger.info(bio)
         try:
             await borg(functions.account.UpdateProfileRequest(  # pylint:disable=E0602
