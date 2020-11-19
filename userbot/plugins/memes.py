290744db96acfcd7db68d438c9a1a9f6aed33737
Sensei <67994849+SenseiOfficial@users.noreply.github.com>
2020-11-19 12:50:42 +0530

    repo part updated

--- userbot/plugins/memes.py
+++ userbot/plugins/memes.py
@@ -850,7 +850,7 @@ async def vapor(vpr):
 @register(outgoing=True, pattern="^.repo$")
 async def source(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        await e.edit("Click [here](https://github.com/spandey112/SensibleUserbot) to open this cool And Best userbot repo By @ceowhitehatcracks.")
+        await e.edit("Click [here](https://github.com/TeamLEGENDX/LegendBOT) to open this cool And Best userbot repo By @legendx22.")
 			  
 			  
 @register(outgoing=True, pattern="^.str(?: |$)(.*)")
