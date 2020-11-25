0586abebaed9f838c2ac271b85b6e55191644de5
sameerbot705 <74921693+sameerbot705@users.noreply.github.com>
2020-11-25 16:35:02 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -12,12 +12,12 @@ from telethon.tl.types import ChannelParticipantsAdmins
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
 PM_IMG = "https://telegra.ph/file/547da1967cf2a4b75daad.mp4"
 pm_caption = "➥ **💥 𝐒𝐀𝐕𝐀𝐆𝐄 𝐁𝐎𝐓 💥 IS:** `ONLINE`\n\n"
-pm_caption += "➥ **👺🐚𝐓𝐄𝐋𝐄𝐓𝐇𝐎𝐍 𝐕𝐄𝐑𝐒𝐈𝐎𝐍  👊♢:** `1.18.0` \n"
+pm_caption += "➥ **👺🐚𝐓𝐄𝐋𝐄𝐓𝐇𝐎𝐍 𝐕𝐄𝐑𝐒𝐈𝐎𝐍:** `1.18.0` \n"
 pm_caption += "➥ **// ✎ 𝐌𝐘 𝐁𝐎𝐒𝐒 ☞ //** \n {DEFAULTUSER} \n"
-pm_caption += "😎 ♕  \n [𝐌𝐘 𝐂𝐇𝐀𝐍𝐍𝐄𝐋] (@savage705)\n\n"
-pm_caption += " 😎 \n [𝐌𝐘 𝐂𝐑𝐄𝐀𝐓𝐎𝐑] (@sameer_705)\n\n"
-pm_caption += " 😎 \n [𝐆𝐑𝐎𝐔𝐏] (https://t.me/joinchat/UQyPTRYXSqTizYtZeCXaZA)\n"
-pm_caption += "[🇮🇳 𝐃𝐄𝐏𝐋𝐎𝐘 𝐘𝐎𝐔𝐑 𝐒𝐀𝐕𝐀𝐆𝐄 𝐁𝐎𝐓 🇮🇳] (https://github.com/sameerbot705/SAVAGEbot)"
+pm_caption += "😎 \n [𝐌𝐘 𝐂𝐇𝐀𝐍𝐍𝐄𝐋](@savage705)\n\n"
+pm_caption += "😎 \n [𝐌𝐘 𝐂𝐑𝐄𝐀𝐓𝐎𝐑](@sameer_705)\n\n"
+pm_caption += "😎 \n [𝐆𝐑𝐎𝐔𝐏](https://t.me/joinchat/UQyPTRYXSqTizYtZeCXaZA)\n"
+pm_caption += "[🇮🇳 𝐃𝐄𝐏𝐋𝐎𝐘 𝐘𝐎𝐔𝐑 𝐒𝐀𝐕𝐀𝐆𝐄 𝐁𝐎𝐓 🇮🇳](https://github.com/sameerbot705/SAVAGEbot)"
 
 
 @borg.on(admin_cmd(pattern=r"alive"))
