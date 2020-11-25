eeb4d7b941b0a46060d83bf21d20dd3b76cf8728
sameerbot705 <74921693+sameerbot705@users.noreply.github.com>
2020-11-25 12:56:35 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -10,17 +10,17 @@ from telethon.tl.types import ChannelParticipantsAdmins
 
 # uptime = get_readable_time((time.time() - Lastupdate))
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
-PM_IMG = "https://telegra.ph/file/db8ece6f805de23a1a534.jpg"
-pm_caption = "➥ **💥SAVAGE BOT💥 IS:** `ONLINE`\n\n"
-pm_caption += "➥ **👺🐚  TÊLÊTHØÑ VÊRẞÏØÑ  👊♢:** `1.18.0` \n"
-pm_caption += "➥ **🄿🅈🅃🄷🄾🄽 ♡☞:** `3.7.4` \n"
-pm_caption += "➥ **👊  𝔡𝔞t𝓪в𝓪ⓢέ  𝕊𝕥𝕒𝕥𝕦𝕤  ♗:**  `Functional`\n"
-pm_caption += f"➥ **// ✎  𝓂𝕪 βỖⓈş  ☞  //** \n {DEFAULTUSER} \n"
+PM_IMG = "https://telegra.ph/file/547da1967cf2a4b75daad.mp4"
+pm_caption = "➥ **💥 𝐒𝐀𝐕𝐀𝐆𝐄 𝐁𝐎𝐓 💥 IS:** `ONLINE`\n\n"
+pm_caption += "➥ **👺🐚𝐓𝐄𝐋𝐄𝐓𝐇𝐎𝐍 𝐕𝐄𝐑𝐒𝐈𝐎𝐍  👊♢:** `1.18.0` \n"
+pm_caption += "➥ ** 𝐏𝐘𝐓𝐇𝐎𝐍 ♡☞:** `3.7.4` \n"
+pm_caption += "➥ **👊  𝐃𝐀𝐓𝐀𝐁𝐀𝐒𝐄 𝐒𝐓𝐀𝐓𝐔𝐒  ♗:**  `Functional`\n"
+pm_caption += f"➥ **// ✎  𝐌𝐘 𝐁𝐎𝐒𝐒  ☞  //** \n {DEFAULTUSER} \n"
 
-pm_caption += " ♕  ⓜ𝕪 𝓒𝓗𝓐𝓝𝓝𝓔𝓛  ♕ 😎 \n [CHANNEL]()\n\n"
-pm_caption += " Ⓜ️𝓎 ℂＲ𝑒Ａ𝕋Øⓡ 😎 \n [SAMEER](@SAMEER_705)\n\n"
-pm_caption += " 𝑀𝒴 𝒢𝑅❁𝒰� 😎 \n [GROUP](@SAVAGEuserbot)\n"
-pm_caption += "[🇮🇳 Deploy your SAVAGE bot🇮🇳](https://github.com/legendx22/LegendBOT)"
+pm_caption += " ♕ 𝐌𝐘 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 ♕ 😎 \n [CHANNEL]()\n\n"
+pm_caption += " 𝐌𝐘 𝐂𝐑𝐄𝐀𝐓𝐎𝐑 😎 \n [SAMEER] ( @SAMEER_705 )\n\n"
+pm_caption += " 𝐆𝐑𝐎𝐔𝐏 😎 \n [GROUP]( @SAVAGEuserbot )\n"
+pm_caption += "[🇮🇳 𝐃𝐄𝐏𝐋𝐎𝐘 𝐘𝐎𝐔𝐑 𝐒𝐀𝐕𝐀𝐆𝐄 𝐁𝐎𝐓 🇮🇳](https://github.com/legendx22/LegendBOT)"
 
 
 @borg.on(admin_cmd(pattern=r"alive"))
