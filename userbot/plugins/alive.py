6423d82ad63d8062ab4e4093a3fff312ae9c6894
ahirearyan2 <66762638+ahirearyan2@users.noreply.github.com>
2020-11-25 17:47:30 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -15,11 +15,9 @@ pm_caption = "➥ **💥 𝐒𝐀𝐕𝐀𝐆𝐄 𝐁𝐎𝐓 💥 IS:** `ONLIN
 pm_caption += "➥ ** 𝐓𝐄𝐋𝐄𝐓𝐇𝐎𝐍 𝐕𝐄𝐑𝐒𝐈𝐎𝐍:** `1.18.0` \n"
 pm_caption += "➥ **// 𝐌𝐘 𝐁𝐎𝐒𝐒 ☞ //** \n {DEFAULTUSER} \n"
 pm_caption += "😎👇👇 \n [𝐌𝐘 𝐂𝐑𝐄𝐀𝐓𝐎𝐑](@sameer_705)\n\n"
-pm_caption += "😎👇👇 \n [𝐒𝐎𝐂𝐈𝐀𝐋 𝐆𝐑𝐎𝐔𝐏](https://t.me/joinchat/UQyPTVfUnFWiNzd2DaHnfA)\n\n"
-pm_caption += "😎👇👇 \n [𝐒𝐔𝐏𝐏𝐎𝐑𝐓 𝐆𝐑𝐎𝐔𝐏](https://t.me/joinchat/UQyPTRYXSqTizYtZeCXaZA)\n"
-
-
-pm_caption += "[🇮🇳 𝐃𝐄𝐏𝐋𝐎𝐘 𝐘𝐎𝐔𝐑 𝐒𝐀𝐕𝐀𝐆𝐄 🔥 𝐁𝐎𝐓 🇮🇳](https://github.com/sameerbot705/SAVAGEbot)
+pm_caption += "😎👇👇 \n [𝐒𝐎𝐂𝐈𝐀𝐋 𝐆𝐑𝐎𝐔𝐏](https://t.me/friendsforever705)\n\n"
+pm_caption += "😎👇👇 \n [𝐒𝐔𝐏𝐏𝐎𝐑𝐓 𝐆𝐑𝐎𝐔𝐏](https://t.me/savage0705)\n"
+pm_caption += "[🇮🇳 𝐃𝐄𝐏𝐋𝐎𝐘 𝐘𝐎𝐔𝐑 𝐒𝐀𝐕𝐀𝐆𝐄 🔥 𝐁𝐎𝐓 🇮🇳](https://github.com/sameerbot705/SAVAGEbot)"
 
 @borg.on(admin_cmd(pattern=r"alive"))
 async def friday(alive):
