7389bb6f9a514a5e961330fb5b2421c076e1658e
ahirearyan2 <66762638+ahirearyan2@users.noreply.github.com>
2020-11-25 18:41:10 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -24,7 +24,7 @@ pm_caption += "** 𝐓𝐄𝐋𝐄𝐓𝐇𝐎𝐍 𝐕𝐄𝐑𝐒𝐈𝐎𝐍:
 pm_caption += f"**//👇👇𝐌𝐘 𝐁𝐎𝐒𝐒 ☞ //** \n {DEFAULTUSER} \n"
 
 
-pm_caption += " \n [𝐌𝐘 𝐂𝐑𝐄𝐀𝐓𝐎𝐑](@sameer_705)\n\n"
+pm_caption += "👇👇 \n [𝐌𝐘 𝐂𝐑𝐄𝐀𝐓𝐎𝐑](@sameer_705)\n\n"
 
 
 pm_caption += "👇👇 \n [𝐒𝐎𝐂𝐈𝐀𝐋 𝐆𝐑𝐎𝐔𝐏](https://t.me/friendsforever705)\n\n"
