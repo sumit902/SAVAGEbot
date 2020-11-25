d862e46f649205e7634cb88ade291dd9476c8fed
sameerbot705 <74921693+sameerbot705@users.noreply.github.com>
2020-11-25 13:49:44 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -13,14 +13,12 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
 PM_IMG = "https://telegra.ph/file/547da1967cf2a4b75daad.mp4"
 pm_caption = "➥ **💥 𝐒𝐀𝐕𝐀𝐆𝐄 𝐁𝐎𝐓 💥 IS:** `ONLINE`\n\n"
 pm_caption += "➥ **👺🐚𝐓𝐄𝐋𝐄𝐓𝐇𝐎𝐍 𝐕𝐄𝐑𝐒𝐈𝐎𝐍  👊♢:** `1.18.0` \n"
-pm_caption += "➥ ** 𝐏𝐘𝐓𝐇𝐎𝐍 ♡☞:** `3.7.4` \n"
-pm_caption += "➥ **👊  𝐃𝐀𝐓𝐀𝐁𝐀𝐒𝐄 𝐒𝐓𝐀𝐓𝐔𝐒  ♗:**  `Functional`\n"
-pm_caption += f"➥ **// ✎  𝐌𝐘 𝐁𝐎𝐒𝐒  ☞ //** \n {DEFAULTUSER} \n"
+pm_caption += f"➥ **// ✎ 𝐌𝐘 𝐁𝐎𝐒𝐒 ☞ \n" {DEFAULTUSER} \n"
 
-pm_caption += " ♕ 𝐌𝐘 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 ♕ 😎 \n [CHANNEL]()\n\n"
-pm_caption += " 𝐌𝐘 𝐂𝐑𝐄𝐀𝐓𝐎𝐑 😎 \n [SAMEER] \n\n"
-pm_caption += " 𝐆𝐑𝐎𝐔𝐏 😎 \n [GROUP]( @SAVAGEuserbot )\n"
-pm_caption += "[🇮🇳 𝐃𝐄𝐏𝐋𝐎𝐘 𝐘𝐎𝐔𝐑 𝐒𝐀𝐕𝐀𝐆𝐄 𝐁𝐎𝐓 🇮🇳](https://github.com/legendx22/LegendBOT)"
+pm_caption += "😎 ♕  \n [𝐌𝐘 𝐂𝐇𝐀𝐍𝐍𝐄𝐋] (@savage705)\n\n"
+pm_caption += " 😎 \n [𝐌𝐘 𝐂𝐑𝐄𝐀𝐓𝐎𝐑] (@sameer_705)\n\n"
+pm_caption += " 😎 \n [𝐆𝐑𝐎𝐔𝐏] (https://t.me/joinchat/UQyPTRYXSqTizYtZeCXaZA)\n"
+pm_caption += "[🇮🇳 𝐃𝐄𝐏𝐋𝐎𝐘 𝐘𝐎𝐔𝐑 𝐒𝐀𝐕𝐀𝐆𝐄 𝐁𝐎𝐓 🇮🇳] (https://github.com/sameerbot705/SAVAGEbot)"
 
 
 @borg.on(admin_cmd(pattern=r"alive"))
